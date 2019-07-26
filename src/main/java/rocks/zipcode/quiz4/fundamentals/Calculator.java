d41b9fce20882e0a09119ac59db23ecf917acc29
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-3.local>
2019-07-26 19:19:08 -0400

    Calculator, all tests passing

--- src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
+++ src/main/java/rocks/zipcode/quiz4/fundamentals/Calculator.java
@@ -1,35 +1,37 @@
 package rocks.zipcode.quiz4.fundamentals;
 
+import java.util.stream.Stream;
+
 /**
  * @author leon on 21/12/2018.
  */
 public class Calculator {
     public static Double squareRoot(Double value) {
-        return null;
+        return Math.sqrt(value);
     }
 
     public static Double square(Double value) {
-        return null;
+        return value * value;
     }
 
     public static Double[] squareRoots(Double... value) {
-        return null;
+        return Stream.of(value).map(Math::sqrt).toArray(Double[]::new);
     }
 
     public static Double[] squares(Double... values) {
-        return null;
+        return Stream.of(values).map(x -> x*x).toArray(Double[]::new);
     }
 
     public static Double add(Double value1, Double value2) {
-        return null;
+        return value1 + value2;
     }
 
     public static Double subtract(Double value1, Double value2) {
-        return null;
+        return value1 - value2;
     }
 
 
     public static Double divide(Double divisor, Double dividend) {
-        return null;
+        return divisor / dividend;
     }
 }
